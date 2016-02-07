82459a584c4fe619a480baa7f38acd9e771af3d2
Hassan Ali <helfi92@gmail.com>
2016-02-07 18:26:31 -0500

    add navbar jquery active class transition

--- controller.js
+++ controller.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+	
+	//set navbar active tab
+	$(".nav a").on("click",function(){
+		$(".nav .active").removeClass("active");
+		$(this).parent().addClass("active");
+	});
 
 });
