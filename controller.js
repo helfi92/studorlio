b3bee63bf30cc2a4c7bacea4d66c85fd04efa5a3
Hassan Ali <helfi92@gmail.com>
2016-03-27 17:41:47 -0400

    remove jquery dependency; remove colors;

--- controller.js
+++ controller.js
@@ -1,10 +1,8 @@
-$(document).ready(function(){
-	
-	//set navbar active tab
-	$(".nav a").on("click",function(){
-		$(".nav .active").removeClass("active");
-		$(this).parent().addClass("active");
-	});
+var navigation_elems = document.querySelectorAll(".nav a");
+for(var i = 0 ; i < navigation_elems.length ; i++){
+	navigation_elems[i].addEventListener("click",function(event){
+		document.querySelector(".nav .active").classList.remove("active");
+		this.parentElement.classList.add("active");
+	})
+}		
 
-	
-});
