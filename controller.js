5755c113cd47086fa88a2101ca008729eec6707b
helfi92 <helfi92@gmail.com>
2017-02-04 23:36:59 -0500

    Change theme

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
 var navigation_elems = document.querySelectorAll(".nav a");
+
 for(var i = 0 ; i < navigation_elems.length ; i++){
 	navigation_elems[i].addEventListener("click",function(event){
 		document.querySelector(".nav .active").classList.remove("active");
