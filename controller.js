97085c429e2b1563cfbf004cc957d65dc7b0e909
helfi92 <helfi92@gmail.com>
2017-02-05 20:46:20 -0500

    Make site responsive

--- controller.js
+++ controller.js
@@ -1,9 +1,11 @@
-var navigation_elems = document.querySelectorAll(".nav a");
+window.onload = () => {
+  const navMenu = document.querySelector('.nav-menu');
+  const navItems = document.querySelectorAll('.nav-item');
+  const hamburger = document.querySelector('.nav-toggle');
+  
+  const toggle = e => e.classList.toggle('is-active');
+  const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
 
-for(var i = 0 ; i < navigation_elems.length ; i++){
-	navigation_elems[i].addEventListener("click",function(event){
-		document.querySelector(".nav .active").classList.remove("active");
-		this.parentElement.classList.add("active");
-	})
-}		
-
+  hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
+  Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
+}
