8a16239315cb88a2782c5d2bc5986102bd3afd62
Jarett Dewbury <89553144+Jdewbury@users.noreply.github.com>
2022-01-20 02:25:30 -0500

    Add files via upload

--- controller.js
+++ controller.js
@@ -1,11 +1,32 @@
 window.onload = () => {
-  const navMenu = document.querySelector('.nav-menu');
-  const navItems = document.querySelectorAll('.nav-item');
-  const hamburger = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+    const navItems = document.querySelectorAll('.nav-item');
+    const hamburger = document.querySelector('.nav-toggle');
+    
+    const toggle = e => e.classList.toggle('is-active');
+    const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
   
-  const toggle = e => e.classList.toggle('is-active');
-  const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
+    hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
+    Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
 
-  hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
-  Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
-}
+    var text = document.getElementById('text');
+    var newDom = '';
+    var animationDelay = 6;
+
+    for(let i = 0; i < text.innerText.length; i++)
+    {
+        newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i])+ '</span>';
+    }
+
+    text.innerHTML = newDom;
+    var length = text.children.length;
+
+    for(let i = 0; i < length; i++)
+    {
+        text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
+    }
+
+    AOS.init({
+        duration: 1200,
+      })
+  }
