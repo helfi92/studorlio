526012b610b55dd0c8bb3c60a066b64b4c7ea1c4
AlejandroPinto5 <alejopintirris@gmail.com>
2023-05-11 10:38:46 -0500

    Filter not ready, but a quiet progress is there

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,101 +1,56 @@
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
 
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-  }
-}
+/*  Javascript filter
+---------------------------------*/
+// animate divs on start
+var items = document.querySelectorAll('.filter-sections div');
+animate(items);
 
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+// filter on click
+each('.filter-links li a', function(el) {
+  el.addEventListener('click', function(e) {
+    e.preventDefault();
+    filterLinks(el);
   });
-}
+});
 
-
-/*====================================
-Portfolio Isotope Filter
-======================================*/
-// $(window).load(function() {
-//     var $container = $('.portfolio-items');
-//     $container.isotope({
-//         filter: '*',
-//         animationOptions: {
-//             duration: 750,
-//             easing: 'linear',
-//             queue: false
-//         }
-//     });
-//     $('.cat a').click(function() {
-//         $('.cat .active').removeClass('active');
-//         $(this).addClass('active');
-//         var selector = $(this).attr('data-filter');
-//         $container.isotope({
-//             filter: selector,
-//             animationOptions: {
-//                 duration: 750,
-//                 easing: 'linear',
-//                 queue: false
-//             }
-//         });
-//         return false;
-//     });
-//
-// });
-
-
-
-    // ====================== Filtering ======================
-    //
-    // _filter : function( $atoms ) {
-    //   var filter = this.options.filter === '' ? '*' : this.options.filter;
-    //
-    //   if ( !filter ) {
-    //     return $atoms;
-    //   }
-    //
-    //   var hiddenClass    = this.options.hiddenClass,
-    //       hiddenSelector = '.' + hiddenClass,
-    //       $hiddenAtoms   = $atoms.filter( hiddenSelector ),
-    //       $atomsToShow   = $hiddenAtoms;
-    //
-    //   if ( filter !== '*' ) {
-    //     $atomsToShow = $hiddenAtoms.filter( filter );
-    //     var $atomsToHide = $atoms.not( hiddenSelector ).not( filter ).addClass( hiddenClass );
-    //     this.styleQueue.push({ $el: $atomsToHide, style: this.options.hiddenStyle });
-    //   }
-    //
-    //   this.styleQueue.push({ $el: $atomsToShow, style: this.options.visibleStyle });
-    //   $atomsToShow.removeClass( hiddenClass );
-    //
-    //   return $atoms.filter( filter );
-    // },
+// filter links functions
+function filterLinks(element) {
+  // get text
+  var el = element.textContent,
+    // convert to lowercase
+    linksTolowerCase = el.toLowerCase();
+  // if all remove all elements
+  if (el === 'All') {
+    // first show all view class
+    each('.view', function(e) {
+      e.classList.remove('view');
+    });
+    // no show init animation
+    animate(items);
+  } else {
+    // if not click all remove all elements
+    each('.view', function(e) {
+      e.classList.remove('view');
+    });
+  }
+  // show animation for current elements
+  animate(document.querySelectorAll('.' + linksTolowerCase));
+};
+// forech arrays
+function each(el, callback) {
+  var allDivs = document.querySelectorAll(el),
+    alltoArr = Array.prototype.slice.call(allDivs);
+  Array.prototype.forEach.call(alltoArr, function(selector, index) {
+    if (callback) return callback(selector);
+  });
+};
+// animate function
+function animate(item) {
+  (function show(counter) {
+    setTimeout(function() {
+      item[counter].classList.add('view');
+      counter++;
+      if (counter < item.length) show(counter);
+    },50);
+  })(0);
+};
