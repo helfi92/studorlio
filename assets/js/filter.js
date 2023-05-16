f7fd9bf914055af60752fd67cb91725d9ec2a952
AlejandroPinto5 <alejopintirris@gmail.com>
2023-05-16 10:49:29 -0500

    Finally filter works, thank to https://codepen.io/modog/pen/eYmvdJe

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,56 +1,54 @@
 
-/*  Javascript filter
----------------------------------*/
-// animate divs on start
-var items = document.querySelectorAll('.filter-sections div');
-animate(items);
+let sortOptions = document.getElementById('sort-options')
+let btns = sortOptions.getElementsByClassName('btn')
 
-// filter on click
-each('.filter-links li a', function(el) {
-  el.addEventListener('click', function(e) {
-    e.preventDefault();
-    filterLinks(el);
-  });
-});
+function filterSelection(str) {
+	let items = document.getElementsByClassName('filterItems')
 
-// filter links functions
-function filterLinks(element) {
-  // get text
-  var el = element.textContent,
-    // convert to lowercase
-    linksTolowerCase = el.toLowerCase();
-  // if all remove all elements
-  if (el === 'All') {
-    // first show all view class
-    each('.view', function(e) {
-      e.classList.remove('view');
-    });
-    // no show init animation
-    animate(items);
-  } else {
-    // if not click all remove all elements
-    each('.view', function(e) {
-      e.classList.remove('view');
-    });
+	if(str == "all"){ str = "" }
+
+	Array.from(items).forEach(item => {
+		removeClass(item, 'show')
+		if(item.className.indexOf(str) > -1){
+			addClass(item, 'show')
+			console.log("working")
+		}
+	})
+}
+
+function addClass(element, name){
+		let itemClass, itemName
+
+		itemClass = element.className.split(' ')
+		itemName = name.split(' ')
+
+		for(let i = 0; i < itemName.length; i++){
+			if(itemClass.indexOf(itemName[i]) == -1){
+				element.className += " " + itemName[i]
+			}
+		}
+}
+
+function removeClass(element, name){
+	let itemClass, itemName;
+
+	itemClass = element.className.split(' ')
+	itemName = name.split(' ')
+
+ for (i = 0; i < itemName.length; i++) {
+    while (itemClass.indexOf(itemName[i]) > -1) {
+      itemClass.splice(itemClass.indexOf(itemName[i]), 1);
+    }
   }
-  // show animation for current elements
-  animate(document.querySelectorAll('.' + linksTolowerCase));
-};
-// forech arrays
-function each(el, callback) {
-  var allDivs = document.querySelectorAll(el),
-    alltoArr = Array.prototype.slice.call(allDivs);
-  Array.prototype.forEach.call(alltoArr, function(selector, index) {
-    if (callback) return callback(selector);
-  });
-};
-// animate function
-function animate(item) {
-  (function show(counter) {
-    setTimeout(function() {
-      item[counter].classList.add('view');
-      counter++;
-      if (counter < item.length) show(counter);
-    },50);
-  })(0);
-};
+  element.className = itemClass.join(" ")
+}
+
+Array.from(btns).forEach((btn) => {
+	btn.addEventListener('click', () => {
+		let current = document.getElementsByClassName('active')
+		current[0].className = current[0].className.replace(' active', '')
+		this.className += ' active';
+	})
+})
+
+filterSelection("all")
